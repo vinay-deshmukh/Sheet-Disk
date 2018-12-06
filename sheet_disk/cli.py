787afd343242323338ea9a49b8f09f886e1e0467
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-12-06 20:06:00 +0530

    add cli interface to package

---
+++ sheet_disk/cli.py
@@ -0,0 +1,7 @@
+'''Wrapper file around the main function 
+in sheet_disk, so module can be called from Command Line Interface'''
+
+from .sheet_disk import main
+
+if __name__ == '__main__':
+    main()
