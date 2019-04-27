8948afe870c92a2a85ea5c281c66ce9ff15290c2
vinay-deshmukh <vinay_deshmukh@outlook.com>
2019-04-27 18:13:57 +0530

    create 0.1.1

--- sheet_disk/__version__.py
+++ sheet_disk/__version__.py
@@ -1,7 +1,7 @@
 
 __title__ = 'sheet_disk'
 __description__ = 'Use Google Sheets as a storage device!'
-__version__ = '0.1'
+__version__ = '0.1.1'
 __author__ = 'Vinay Deshmukh'
 __author_email__ = 'vinay.deshmukh.official@outlook.com'
 __license__ = 'MIT'
